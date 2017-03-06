6d9f7c10491e5a2f0f6e51e2d1c0974977f6a3de
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-06 14:44:56 +0000

    Add default date of today's date for new transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,9 +4,13 @@ class Transaction
 
   attr_reader :amount, :date
 
-  def initialize(amount, date = Date.today.strftime("%d/%m/%Y"))
+  def initialize(amount, date=nil)
     @amount = amount
-    @date = date
+    @date = get_date(date)
+  end
+
+  def get_date(date)
+    date ? date :  Date.today.strftime("%d/%m/%Y")
   end
 
 end
