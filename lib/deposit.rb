0bed651cb2290dd0b90ab91dfbcee04f00e8ee52
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-06 12:54:20 +0000

    Fix spacing in Deposit class

--- lib/deposit.rb
+++ lib/deposit.rb
@@ -4,7 +4,7 @@ class Deposit
 
   attr_reader :amount, :date
 
-  def initialize(amount, date=Date.today.strftime("%d/%m/%Y"))
+  def initialize(amount, date = Date.today.strftime("%d/%m/%Y"))
     @amount = amount
     @date = date
   end
